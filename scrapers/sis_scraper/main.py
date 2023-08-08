e248f9d51d6731d775630d461c51e8c35e62e6fa
powe97 <116031952+powe97@users.noreply.github.com>
2023-08-07 22:17:03 -0400

    Fix catalog scraping crashing when term is empty

--- scrapers/sis_scraper/main.py
+++ scrapers/sis_scraper/main.py
@@ -334,6 +334,7 @@ async def scrape_term(term):
 
 async def scrape_term_catalog(term):
     print(f"Scraping {term} catalog...")
+    os.makedirs(f"data/{term}", exist_ok=True)
     global session
     url = f"https://sis.rpi.edu/rss/bwckctlg.p_display_courses?term_in={term}&sel_crse_strt=&sel_crse_end=&sel_subj=&sel_levl=&sel_schd=&sel_coll=&sel_divs=&sel_dept=&sel_attr="
     async with session.get(url) as request:
